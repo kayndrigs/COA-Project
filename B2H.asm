4de829906d868a3164070403bd1fd3780d973908
Kayne <103131412+kayndrigs@users.noreply.github.com>
2023-05-26 07:12:17 +0800

    Update B2H.asm

--- B2H.asm
+++ B2H.asm
@@ -1,122 +1,134 @@
+;Binary to Hex
+
+print macro str 
+    mov ah, 09h 
+    lea dx, str
+    int 21h
+    endm
+
+
 .model small
 .stack 100h
 .data
     ; messages
-    msgstar     db 0dh, 0ah, 0dh, 0ah, "*******************************************************************************", 0dh, 0ah, "$"
-    msgintro    db "PROGRAM FUNCTION", 0dh, 0ah, "TO CONVERT 4-BIT BINARY NUMBER INTO HEXADECIMAL NUMBER", "$"
-    msginput    db 0dh, 0ah, 0dh, 0ah, "Enter a 4-Bit Binary Number: $"
+    msginput    db "Enter a 8-Bit Binary Number (0-1, append trailing zeroes): $"
+    lfcr        db 0dh, 0ah, "$"
     msgafter    db "Hexadecimal Conversion: $"
-    msgagain    db 0dh, 0ah, 0dh, 0ah, "Do you want to try again? (Y/N): $", 0dh, 0ah, "$"
     msginvalid  db 0dh, 0ah, "Invalid input! Please enter a valid 4-bit binary number.", 0dh, 0ah, "$"
-    msgexit     db 0dh, 0ah, "Exiting the program...", 0dh, 0ah, "$"
+    temp dw 3 dup(?)
 
 .code
 main proc
     mov ax, @data
     mov ds, ax
 
-    mov dx, offset msgstar ; display start (decoration)
-    mov ah, 9
-    int 21h
+    mov cx, 0
+    input_loop:
+        mov dx, offset msginput ; display input message
+        mov ah, 9
+        int 21h
 
-    mov dx, offset msgintro ; display intro message
-    mov ah, 9
-    int 21h
-
-    mov dx, offset msgstar ; display start (decoration)
-    mov ah, 9
-    int 21h
-
-input_loop:
-    mov dx, offset msginput ; display input message
-    mov ah, 9
-    int 21h
-
-    mov cx, 4 ; set maximum 4 bits
-
-    xor bx, bx ; clear BX register for storing the binary number
-    xor dx, dx ; clear DX register for hexadecimal conversion
-
-input:
-    mov ah, 01h ; read a character
-    int 21h
-
-    cmp al, 0Dh ; check for Enter key
-    je convert
-
-    cmp al, '0' ; compare with '0'
-    je set_bit_0
-    cmp al, '1' ; compare with '1'
-    je set_bit_1
-
-    jmp invalid_input
-
-set_bit_0:
-    shl bx, 1 ; shift left by 1
-    jmp input
-
-set_bit_1:
-    shl bx, 1 ; shift left by 1
-    or bx, 1 ; set the lowest bit to 1
-    jmp input
-
-convert:
-    mov dl, bl ; copy the 4-bit segment to DL register
-
-    cmp dl, 9 ; check if it's 0-9
-    jbe convert_digit
-    add dl, 7 ; convert to A-F
-
-convert_digit:
-    add dl, 30h ; convert to ASCII character
-
-    jmp try_again
-
-try_again:
-    mov dx, offset msgafter ; display hexadecimal conversion message
-    mov ah, 9
-    int 21h
-
-    mov dl, bl ; copy the 4-bit segment to DL register
-
-    cmp dl, 9 ; check if it's 0-9
-    jbe convert_digit_after
-    add dl, 7 ; convert to A-F
-
-convert_digit_after:
-    add dl, 30h ; convert to ASCII character
-
-    mov ah, 02h ; print character
-    int 21h
-
-    jmp ask_try_again
-
-ask_try_again:
-    mov dx, offset msgagain ; prompt to try again
-    mov ah, 9
-    int 21h
-
-    mov ah, 01h ; read a key
-    int 21h
-
-    cmp al, 'Y'
-    je input_loop
-
-exit_program:
-    mov dx, offset msgexit ; display exit message
-    mov ah, 9
-    int 21h
-
-    mov ah, 4Ch ; terminate program function
-    mov al, 0 ; return code
-    int 21h
-
-invalid_input:
-    mov dx, offset msginvalid ; display invalid input message
-    mov ah, 9
-    int 21h
-
-    jmp ask_try_again
-
+        xor bx, bx ; clear BX register for storing the binary number
+        xor dx, dx ; clear DX register for hexadecimal conversion 
+        lea si, temp
+    
+    input:
+        mov ah, 01h ; read a character
+        int 21h
+    
+        cmp al, 0Dh ; check for Enter key
+        je pass_block
+    
+        cmp al, '0' ; compare with '0'
+        je set_bit_0
+        cmp al, '1' ; compare with '1'
+        je set_bit_1
+    
+        jmp invalid_input 
+        
+     
+    
+    set_bit_0:
+        inc cx
+        shl bx, 1 ; shift left by 1 
+        cmp cx, 4
+        je push_digits 
+        jmp input
+    
+    set_bit_1:
+        inc cx
+        shl bx, 1 ; shift left by 1
+        or bx, 1 ; set the lowest bit to 1 
+        cmp cx, 4
+        je push_digits 
+        jmp input
+        
+    push_digits:
+        mov [si],bx 
+        inc si
+        mov cx, 00
+        mov bx, 00
+        jmp input
+    
+    pass_block:
+        mov cx, 2
+        lea si, temp
+        passing:
+            mov dx, [si]
+            push dx
+            inc si
+            loop passing
+        jmp continue
+     
+    continue:
+        mov cx, 2
+        print lfcr
+        print msgafter
+    convert:
+        pop bx
+        mov dl, bl ; copy the 8-bit segment to DL register
+    
+        cmp dl, 9 ; check if it's 0-9
+        jbe convert_digit
+        add dl, 7 ; convert to A-F
+    
+    convert_digit:
+        add dl, 30h ; convert to ASCII character
+    
+        jmp try_again
+    
+    try_again:
+       
+        
+        mov dl, bl ; copy the 4-bit segment to DL register
+    
+        cmp dl, 9 ; check if it's 0-9
+        jbe convert_digit_after
+        add dl, 7 ; convert to A-F
+    
+    convert_digit_after:
+        add dl, 30h ; convert to ASCII character
+    
+        mov ah, 02h ; print character
+        int 21h 
+        dec cx 
+        cmp cx, 1
+        je convert
+    
+    
+    exit_program:
+    
+        mov ah, 4Ch ; terminate program function
+        mov al, 0 ; return code
+        int 21h
+     
+    
+    invalid_input:
+        mov dx, offset msginvalid ; display invalid input message
+        mov ah, 9
+        int 21h
+    
+        jmp exit_program
+    
 main endp
 end main
